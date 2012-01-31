4d2b88196a3304420fafdccb8a4db22902a17244
Tass Skoudros <tass@skoudros.com>
2012-01-31 15:38:58 +0000

    first commit
    Signed-off-by: Tass Skoudros <tass@skoudros.com>

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Tass Skoudros"
+maintainer_email "tass@skystack.com"
+license          "Apache 2.0"
+description      "Install and configure the collectd monitoring daemon"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "1.0.0"
+supports         "ubuntu"
